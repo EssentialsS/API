f2ba14843d63402e1a2b6ef34bcc1bcbe349f9d7
mose <nekronmoseley@gmail.com>
2023-07-05 17:24:37 +0100

    Updated to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    `java-library`
+}
+
+group = "org.essentialss"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io")
+    maven("https://repo.spongepowered.org/repository/maven-public/")
+}
+
+dependencies {
+    implementation("com.github.mosemister:DataProperties:master-SNAPSHOT")
+    implementation("org.spongepowered:spongeapi:8.0.0")
+}
+
+val javaTarget = 8 // Sponge targets a minimum of Java 8
+java {
+    sourceCompatibility = JavaVersion.toVersion(javaTarget)
+    targetCompatibility = JavaVersion.toVersion(javaTarget)
+}
+
+tasks.withType(JavaCompile::class).configureEach {
+    options.apply {
+        encoding = "utf-8" // Consistent source file encoding
+        if (JavaVersion.current().isJava10Compatible) {
+            release.set(javaTarget)
+        }
+    }
+}
+
+// Make sure all tasks which produce archives (jar, sources jar, javadoc jar, etc) produce more consistent output
+tasks.withType(AbstractArchiveTask::class).configureEach {
+    isReproducibleFileOrder = true
+    isPreserveFileTimestamps = false
+}
